edcb43cfe53a48a0ff3749f6c5d7c9be4abc3479
takahashi yuuhei <gonbee1230@gmail.com>
2020-12-14 08:29:18 +0900

    fix config.yml and setting.py change circleci ver

--- chatadmin/settings.py
+++ chatadmin/settings.py
@@ -97,7 +97,7 @@ DATABASES = {
         'NAME': 'postgres',
         'USER': 'postgres',
         'PASSWORD': 'postgres',
-        'HOST': 'db',
+        'HOST': 'localhost',
         'PORT': 5432,
     }
 }
